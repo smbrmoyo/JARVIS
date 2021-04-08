e28c50490a7e482e01bab5e734d4ce12b88114fa
smbrmoyo <brianmoyou@gmail.com>
2021-04-08 06:38:09 +0200

    first commit

---
+++ functions.py
@@ -0,0 +1,13 @@
+# All the functionalities of JARVIS
+
+def take_appointment(**kwargs):
+    print('Taking an appointment...')
+
+def call_emergency():
+    print('Calling 911')
+
+def find_hospital():
+    print('Finding hospital...')
+
+call_emergency()
+
---
+++ test.py
@@ -0,0 +1,5 @@
+import numpy as np
+
+players = [1,2,3,4]
+
+players_np = np.array(players)
